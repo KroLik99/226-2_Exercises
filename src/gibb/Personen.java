43f525b918c411a9beda52d1110309ab76f2afde
vmadmin <vmadmin@bmLP1.smartlearn.wan>
2023-05-01 10:45:08 +0200

    aufgabe2 ab2

--- src/gibb/Personen.java
+++ src/gibb/Personen.java
@@ -1,12 +1,10 @@
 package gibb;
 
-public class Aufgabe1_VererbungUndReferenzen {
 
-}
 class Person {
-    String name;
-    String vorname;
-    int personalNummer;
+    private String name;
+    private String vorname;
+    private int personalNummer;
     public Person(String name, String vorname, int personalNummer){
         this.name = name;
         this.vorname = vorname;
@@ -14,7 +12,7 @@ class Person {
     }
 }
 class Chef extends Person {
-    String abteilung;
+    private String abteilung;
     public Chef(String name, String vorname, int personalNummer,String abteilung){
         super(name,vorname,personalNummer); // weil Chef von Person erbt muss er alle Instanzvariabeln von Person im Konstruktor haben
         this.abteilung = abteilung;
@@ -22,7 +20,7 @@ class Chef extends Person {
     }
 }
 class Fachangestellter extends Person {
-    Chef vorgesetzter;
+    protected Chef vorgesetzter; //wir greifen auf diesen zu (ohne Setter)
 
     public Fachangestellter(String name, String vorname, int personalNummer, Chef vorgesetzter) {
         super(name, vorname, personalNummer);
@@ -31,19 +29,20 @@ class Fachangestellter extends Person {
 }
 public class Personen {
     public static void main (String args[]) {
-        Fachangestellter personal1 = new Fachangestellter();
-        Fachangestellter personal2 = new Fachangestellter();
-        Chef personal3 = new Chef();
-        Person personal4 = new Person();
-        personal3.name="Sattler";
-        personal3.vorname="Beatrice";
-        personal3.abteilung="Verkauf";
-        personal1.name="Klein";
-        personal1.vorname="Thomas";
-        personal1.vorgesetzter=personal3; //*1
-        personal4 = personal1; //*2
-        personal4.vorgesetzter = personal3; //*3
-        personal1 = personal4; //*4
+        Chef personal3 = new Chef("Sattler", "Beatrice", 234,"Verkauf");
+
+
+        Fachangestellter personal1 = new Fachangestellter("Klein","Thomas", 123, personal3);
+        Fachangestellter personal2 = new Fachangestellter("Gross", "Thomasa", 456, personal3);
+        Person personal4 = new Person(null, null, 0);
+
+        personal1.vorgesetzter = personal3; //1
+        personal4=personal1;//2
+       /*
+       personal4.vorgesetzter = personal3; //3
+        personal1 = personal4; //4
+        */
+
         personal1 = (Fachangestellter) personal4; //*5
     }
 }
