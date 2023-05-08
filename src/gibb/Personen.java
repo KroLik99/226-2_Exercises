f2f084749a4230a32b4404af4a191e0a36ac4079
vmadmin <vmadmin@bmLP1.smartlearn.wan>
2023-05-08 11:58:58 +0200

    add ferien

--- src/gibb/Personen.java
+++ src/gibb/Personen.java
@@ -1,24 +1,52 @@
 package gibb;
 
 
-class Person {
+abstract class Person {
     protected String name;
     protected String vorname;
     protected int personalNummer;
-    public Person(String name, String vorname, int personalNummer){
+
+    public Person(String name, String vorname, int personalNummer) {
         this.name = name;
         this.vorname = vorname;
         this.personalNummer = personalNummer;
     }
+
+    public void print() { // deklarieren und implementieren
+        System.out.println("Name: " + this.name);
+        System.out.println("Vorname: " + this.vorname);
+        System.out.println("Personalnummer: " + this.personalNummer);
+    }
+
+    public abstract int berechneFerien(int alter); //deklariert aber nicht implementiert -> abstrakte Klasse und keine Objekte mehr möglich von Person (instanciated)
 }
+
 class Chef extends Person {
     protected String abteilung;
-    public Chef(String name, String vorname, int personalNummer,String abteilung){
-        super(name,vorname,personalNummer); // weil Chef von Person erbt muss er alle Instanzvariabeln von Person im Konstruktor haben
+
+    public Chef(String name, String vorname, int personalNummer, String abteilung) {
+        super(name, vorname, personalNummer); // weil Chef von Person erbt muss er alle Instanzvariabeln von Person im Konstruktor haben
         this.abteilung = abteilung;
 
     }
+
+    @Override
+    public void print() { // deklarieren und implementieren, überschreibt print() von der Basisklasse
+        super.print();
+        System.out.println(abteilung);
+    }
+
+    @Override
+    public int berechneFerien(int alter) {
+        if(alter < 45){
+            return 4;
+        } else if (alter>=45 && alter < 55) {
+            return 5;
+        }
+        return 6;
+    }
 }
+
 class Fachangestellter extends Person {
     protected Chef vorgesetzter; //wir greifen auf diesen zu (ohne Setter)
 
@@ -26,15 +54,48 @@ class Fachangestellter extends Person {
         super(name, vorname, personalNummer);
         this.vorgesetzter = vorgesetzter;
     }
+
+    @Override
+    public int berechneFerien(int alter) {
+        if(alter < 50){
+            return 4;
+        } else if (alter>=50 && alter < 60) {
+            return 5;
+        }
+        return 6;
+    }
 }
+
+class Lernender extends Person {
+    private int lernjahr;
+
+    public Lernender(String name, String vorname, int personalNummer, int lernjahr) {
+        super(name, vorname, personalNummer);
+        this.lernjahr = lernjahr;
+    }
+@Override
+    public void print() { // deklarieren und implementieren, überschreibt print() von der Basisklasse
+        super.print();
+        System.out.println("Lernjahr: " + this.lernjahr);
+    }
+
+    @Override
+    public int berechneFerien(int alter) {
+        return 5;
+    }
+}
+
 class Firma {
     public Person[] mitarbeiter;
-    public Firma (Person[] p) {
+
+    public Firma(Person[] p) {
         mitarbeiter = p;
     }
 }
+
 public class Personen {
-    public static void main (String args[]) {
+    public static void main(String args[]) {
+
         /*
         Chef personal3 = new Chef("Sattler", "Beatrice", 234,"Verkauf");
 
@@ -62,19 +123,19 @@ public class Personen {
         a[4] = 5;
 
         int[] b = new int[]{
-               1,2,3,4,5
+                1, 2, 3, 4, 5
         };
 
-        Person[] people = new Person[] {
-                new Chef("Sattler", "Beatrice",25, "Verkauf"),
-                new Fachangestellter("","",0,null)
+        Person[] people = new Person[]{
+                new Chef("Sattler", "Beatrice", 25, "Verkauf"),
+                new Fachangestellter("", "", 0, null)
         };
 
         Firma f = new Firma(
-                new Person[] {
-                        new Chef("Sattler", "Beatrice",25, "Verkauf")
+                new Person[]{
+                        new Chef("Sattler", "Beatrice", 25, "Verkauf")
                 }
-                ); // <==
+        ); // <==
 
         System.out.println(f.mitarbeiter[0].name);
         System.out.println(((Chef) f.mitarbeiter[0]).abteilung);
