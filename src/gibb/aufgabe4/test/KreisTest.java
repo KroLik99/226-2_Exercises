b3c3246843f9ba03bf92b7cf4a69f4c6fd6fa1e1
KroLik99 <73555216+KroLik99@users.noreply.github.com>
2023-06-20 12:59:43 +0200

    Update KreisTest.java
    Mockito text ohne import

--- src/gibb/aufgabe4/test/KreisTest.java
+++ src/gibb/aufgabe4/test/KreisTest.java
@@ -10,13 +10,21 @@ public class KreisTest {
         //Given
         Kreis kreis = new Kreis(60, 40, 25);
         GraphicsStub graphics = new GraphicsStub();
+		// das Gleiche macht Mockito
+		Graphics g = Mockito.mock(Graphics.class); //objekt von Graphics mit Mockito, macht ein g.drawOval() nach 
         //When
         kreis.zeichne(graphics);
+// mockito
+		kreis.zeichne(g)
         //Then
         Assert.assertEquals(1, graphics.numberOfCalls);
         Assert.assertEquals(60-25, graphics.x);
         Assert.assertEquals(40-25, graphics.y);
         Assert.assertEquals(50, graphics.width);
         Assert.assertEquals(50, graphics.height);
+//mockito
+		Mockito.verify(g, Mockito.times(1)).drawOval(60-25,40-25,50,50);
+//Mockito.verify(g, Mockito.times(1)) returns a graphics object: macht alles was Assert macht
+//Kreis Position vom Oval ist oben links und braucht den Durchmesser (breite und höhe)
     }
 }
